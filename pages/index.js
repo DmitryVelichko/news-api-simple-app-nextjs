6ea7a24cbf631dc373de5417f9b75572e276804b
Dmitry Velichko <soleil-2005@mail.ru>
2022-03-29 23:46:48 +0300

    New commit

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-     
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
+    
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}

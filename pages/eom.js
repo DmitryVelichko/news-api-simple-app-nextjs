50037c67b35f5071bf9e669a4e6b40839e2ec358
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-02 23:49:44 +0300

    New commit

--- pages/eom.js
+++ pages/eom.js
@@ -1,9 +1,11 @@
 import link from '../public/link.jpg'
 import styles from '../styles/EOM.module.css'
+import { Toolbar } from '../components/toolbar'
 
 export const EOM = ({ employee }) => {
     return (
         <div className="page-container">
+            <Toolbar />
             <div className={styles.main}>
                 <h1>Employee Of The Month</h1>
 

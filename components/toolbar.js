a2ab799d094c9b4d48baad6a66ef5ae2c0de2aea
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-03 19:40:38 +0300

    New commit

--- components/toolbar.js
+++ components/toolbar.js
@@ -1,15 +1,15 @@
-import { useRouter } from "next/router"
-import styles from '../styles/Toolbar.module.css'
+import { useRouter } from 'next/router';
+import styles from '../styles/Toolbar.module.css';
 
 export const Toolbar = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return(
-        <div className={styles.main}>
-            <div onClick={() => router.push('/')}>Home</div>
-            <div onClick={() => router.push('/feed/1')}>Feed</div>
-            <div onClick={() => router.push('/eom')}>EOM</div>
-            <div onClick={() => window.location.href = 'https://twitter.com'}>Twitter</div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.main}>
+      <div onClick={() => router.push('/')}>Home</div>
+      <div onClick={() => router.push('/feed/1')}>Feed</div>
+      <div onClick={() => router.push('/eom')}>EOM</div>
+      <div onClick={() => (window.location.href = 'https://twitter.com/portexe')}>Twitter</div>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Toolbar } from '../components/toolbar'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import { Toolbar } from '../components/toolbar';
 
 export default function Home() {
+  return (
+    <div className="page-container">
+      <Toolbar />
 
-    return (
-        <div className='page-container'>
+      <div className={styles.main}>
+        <h1>Next.js News App</h1>
 
-        <Toolbar />
-
-            <div className={styles.main}>
-                <h1>Next.js News App</h1>
-
-                <h3>Latest News Articles</h3>
-            </div>
-        </div>
-    )
-  
+        <h3>Your one stop shop for the latest news articles</h3>
+      </div>
+    </div>
+  );
 }

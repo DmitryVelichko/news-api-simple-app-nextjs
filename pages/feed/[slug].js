03eca53bc952d4231170298e8f10fe1c70b12276
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-03 18:47:37 +0300

    New commit

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,5 +1,9 @@
-export const Feed = () => {
+export const Feed = ({ pageNumber, articles }) => {
     return (<>Hello slug</>);
+}
+
+export const getServerSideProps = async pageContext => {
+    
 }
 
 export default Feed;

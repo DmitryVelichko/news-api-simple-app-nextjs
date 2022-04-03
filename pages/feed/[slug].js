7e421ff00f4d389ba715c3a018fa194a46fbcda2
Dmitry Velichko <soleil-2005@mail.ru>
2022-04-03 18:52:21 +0300

    New commit

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -16,6 +16,11 @@ export const getServerSideProps = async pageContext => {
 
     const apiResponse = await fetch (
         `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+            }
+        }
     )
 }
 
